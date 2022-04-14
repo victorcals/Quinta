e3f7efa7b277620efdbdf06b2c4656f02794c572
unknown <victorcals78@gmail.com>
2022-04-13 22:28:53 -0300

    teste

---
+++ projeto_carro/js/jin.js
@@ -0,0 +1,8 @@
+function Toggle() {
+    var temp = document.getElementById('typepass')
+    if (temp.type === 'password') {
+      temp.type = 'text'
+    } else {
+      temp.type = 'password'
+    }
+  }
